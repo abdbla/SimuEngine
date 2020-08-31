6f36d27c1661b74087d207db88b562d914b34126
Sakulsan <Lukasoboy5@gmail.com>
2020-08-31 14:15:21 +0200

    Got monogame FINALLY working
    To get it too work i a new project in the same solution named NodeMonog. This project was prebuilt with vs2017 monogame preset so everything just works. I can also merge it well since it is in a seperate project. I was tunnelled vision into making it a new cs file in the main project, which would have made sense in some ways, but man is this better (since it works and all). So now I can finally start designing and creatign a fitting GUI.

---
+++ SimuEngine/NodeMonog/MegaLoop.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using SimuEngine;
+
+namespace NodeMonog
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class MegaLoop : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont arial;
+        Texture2D circle;
+        Node example;
+        
+        public class ExampleNode : Node
+    {
+        static char ID = 'a';
+        public string name;
+        public ExampleNode() {
+            name = ID++.ToString();
+        }
+
+        public override void OnGenerate()
+        {
+            return;
+        }
+        public override void OnCreate()
+        {
+            return;
+        }
+
+        
+        public void ExampleNode_IEquatable_Success() {
+            ExampleNode n1 = new ExampleNode();
+            ExampleNode n2 = new ExampleNode();
+        }
+    }
+        
+
+        public MegaLoop()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            arial = Content.Load<SpriteFont>(@"Arial");
+            circle = Content.Load<Texture2D>(@"circle");
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            spriteBatch.DrawString(arial, "pls", Vector2.Zero, Color.Black);
+            spriteBatch.Draw(circle, new Rectangle(64, 64, 64, 64), new Rectangle(0,0,64,64),Color.Red);
+            
+
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
