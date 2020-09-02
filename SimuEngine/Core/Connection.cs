03842f4016d1e0eca12cfbda4a926b01737565a5
TerraDOOM <look4totte@gmail.com>
2020-09-02 12:47:12 +0200

    added traits and statuses to connection

--- SimuEngine/Core/Connection.cs
+++ SimuEngine/Core/Connection.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Core
 {
     public abstract class Connection
     {
+        protected List<string> statuses;
+        protected Dictionary<string, int> traits;
     }
 }
