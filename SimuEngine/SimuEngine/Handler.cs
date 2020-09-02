4fdc5553cda4c7d78d91155345dc42c7bbb8d6b4
TerraDOOM <look4totte@gmail.com>
2020-09-02 13:23:47 +0200

    changed up the handler tick method a bit

--- SimuEngine/SimuEngine/Handler.cs
+++ SimuEngine/SimuEngine/Handler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 using Core;
@@ -9,61 +10,19 @@ namespace SimuEngine
     class Handler
     {
         EventListContainer events;
-
+        // TODO: merge documentation
         /// <summary>
         /// Method to tick every node forward one step, triggering events. Requires several calls if several top level nodes exist.
         /// </summary>
         /// <param name="graph">The Graph class created as part of the GraphSystem class. Can be passed a subGraph part of a node to partially tick forward the system.</param>
-        public void Tick(Graph graph)
-        {
-            List<Graph> graphs = new List<Graph>();
-            graphs.Add(graph);
-
-            for (int i = 0; i < graph.Nodes.Count; i++) {
-                List<Event> posEvents = new List<Event>();
-                Random rng = new Random();
-                foreach (Event ev in events.GetEventList(graph.Nodes[i].GetType())) {
-                    bool req = true;
-                    bool pos = true;
-                    var worldGraph = graphs[0];
-                    var localGraph = graphs[graphs.Count - 1];
-                    for (int j = 0; j < ev.ReqGuaranteed.Count; j++) {
-                        if (!ev.ReqGuaranteed[i](graph.Nodes[i], localGraph, worldGraph)) {
-                            req = false;
-                        }
-                    }
-                    if (req) {
-                        foreach (var act in ev.Outcome) {
-                            act(graph.Nodes[i], localGraph, worldGraph);
-                        }
-                    } else {
-                        for (int j = 0; j < ev.ReqGuaranteed.Count; j++) {
-                            if (!ev.ReqPossible[i](graph.Nodes[i], localGraph, worldGraph)) {
-                                pos = false;
-                            }
-                        }
-                    }
-                    if (pos) {
-                        if (rng.NextDouble() <= ev.Chance) {
-                            foreach (var act in ev.Outcome) {
-                                act(graph.Nodes[i], localGraph, worldGraph);
-                            }
-                        }
-                    }
-                }
-
-                if (graph.Nodes[i].Graph != null) {
-                    Tick(graph.Nodes[i].Graph, graphs);
-                }
-            }
-        }
         /// <summary>
         /// Internal function to recursively iterate over several subgraphs.
         /// </summary>
         /// <param name="graph">the subgraph on which to iterate over</param>
         /// <param name="graphTree">the current graphs it has gone through</param>
-        private void Tick(Graph graph, List<Graph> graphTree)
+        public void Tick(Graph graph, List<Graph> graphTree = null)
         {
+            graphTree ??= new List<Graph>();
             List<Graph> graphs = new List<Graph>();
             graphs.AddRange(graphTree);
             graphs.Add(graph);
