166fdd7f363cf78479b57775bc0ea4e90bef5755
TerraDOOM <look4totte@gmail.com>
2020-09-11 12:40:24 +0200

    Added the System.Text.Json package, wrote a (very) naive serializer.

--- SimuEngine/SimuEngine/GraphSystem.cs
+++ SimuEngine/SimuEngine/GraphSystem.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 using Core;
 
@@ -28,6 +31,20 @@ namespace SimuEngine
             T node = new T();
             graph.Add(node);
             node.OnCreate();
+        }
+
+        public void Serialize(string fileName) {
+            using (FileStream fs = File.Create(fileName)) {
+                var json = JsonSerializer.Serialize(this);
+                File.WriteAllText(fileName, json);
+            }
+        }
+
+        public static GraphSystem Deserialize(string fileName) {
+            using (FileStream fs = File.OpenRead(fileName)) {
+                var json = File.ReadAllText(fileName);
+                return JsonSerializer.Deserialize<GraphSystem>(json);
+            }
         }
     }
 }
