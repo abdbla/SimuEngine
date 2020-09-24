31b2efe94ad54da0a85a1b13429b86d05945767e
TerraDOOM <look4totte@gmail.com>
2020-09-24 20:11:16 +0200

    Added the Force-based graph layout, +methods to Graph and Connection
    Added Physics.cs together with Core.Physics, which handles simulating
    the physics system for use in making a layout.
    
    Added a property Connections to Graph for simply enumerating all the
    existing connections.
    
    Added methods to Graph, namely:
    - (private) FindNodeIndex
    - TryGetDirectedConnection
    - GetNeighbors
    
    Added a virtual Strength method to Connection, to give a numerical
    indicator of the overall strength the Connection has.

--- SimuEngine/Core/Connection.cs
+++ SimuEngine/Core/Connection.cs
@@ -5,5 +5,7 @@ namespace Core {
     public abstract class Connection {
         protected List<string> statuses;
         protected Dictionary<string, int> traits;
+
+        public virtual float Strength() => 0.0f;
     }
 }
