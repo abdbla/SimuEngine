85ea3697e4b7e11412dab5ce57b6a13b798c37e2
TerraDOOM <look4totte@gmail.com>
2020-09-24 20:11:16 +0200

    Added the Force-based graph layout, +methods to Graph and Connection
    Added Physics.cs together with Core.Physics, which handles simulating
    the physics system for use in making a layout.
    
    Added a property Connections to Graph for simply enumerating all the
    existing connections.
    
    Added methods to Graph, namely:
    - (private) FindNodeIndex
    - TryGetDirectedConnection
    - GetNeighbors
    
    Added a virtual Strength method to Connection, to give a numerical
    indicator of the overall strength the Connection has.

---
+++ SimuEngine/Core/Physics.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Diagnostics;
+
+namespace Core.Physics {
+    [DebuggerDisplay("Position: {Point.Position.X} {Point.Position.Y}")]
+    public class PhysicsNode : Node {
+        public float Mass { get; set; }
+        public Point Point;
+        public Node Inner { get; private set; }
+        private static Random rng = new Random();
+
+        public PhysicsNode(Node node) {
+            var x = (float)((rng.NextDouble() - .5) * 10.0);
+            var y = (float)((rng.NextDouble() - .5) * 10.0);
+            Inner = node;
+            Mass = 1.0f;
+            Point = new Point(new Vector2(x, y));
+        }
+
+        public override void OnCreate() {
+            Inner.OnCreate();
+        }
+
+        public override void OnGenerate() {
+            Inner.OnGenerate();
+        }
+
+        public void ApplyForce(Vector2 force) {
+            Point.Acceleration += force / Mass;
+        }
+    }
+
+    public struct Vector2 {
+        public static readonly Vector2 Zero = new Vector2(0, 0);
+
+        public float X;
+        public float Y;
+
+        public Vector2(float x, float y) {
+            X = x;
+            Y = y;
+        }
+
+        public static Vector2 operator +(Vector2 v, Vector2 u) => new Vector2(v.X + u.X, v.Y + u.Y);
+
+        public static Vector2 operator -(Vector2 v, Vector2 u) => new Vector2(v.X - u.X, v.Y - u.Y);
+
+        public static Vector2 operator -(Vector2 v) => new Vector2(-v.X, -v.Y);
+
+        public static Vector2 operator *(Vector2 v, Vector2 u) => new Vector2(v.X * u.X, v.Y * u.Y);
+
+        public static Vector2 operator *(Vector2 v, float s) => new Vector2(v.X * s, v.Y * s);
+
+        public static Vector2 operator /(Vector2 v, float s) => new Vector2(v.X / s, v.Y / s);
+
+        public float Magnitude() {
+            return (float)Math.Sqrt(X * X + Y * Y);
+        }
+
+        public Vector2 Normalize() {
+            float m = Magnitude();
+            return this * (1 / m);
+        }
+    }
+
+    public class Point {
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public Vector2 Acceleration;
+
+        public Point(Vector2 pos, Vector2 vel, Vector2 acc) {
+            Position = pos;
+            Velocity = vel;
+            Acceleration = acc;
+        }
+
+        public Point(Vector2 pos) {
+            Position = pos;
+            Velocity = Acceleration = Vector2.Zero;
+        }
+    }
+
+    class Spring : Connection {
+        public PhysicsNode n1;
+        public PhysicsNode n2;
+        public float Length;
+        public float K;
+
+        public Spring(PhysicsNode n1, PhysicsNode n2, float length, float k) {
+            Length = length;
+            K = k;
+        }
+
+        public override float Strength() => 0.0f;
+    }
+
+    public class System {
+        public float Stiffness;
+        public float Repulsion;
+        public float Damping;
+        public float Threshold;
+        public float Gravity;
+        public bool WithinThreshold;
+
+        public Graph Graph { get; private set; }
+        public Dictionary<Node, PhysicsNode> physicsNodes;
+
+        public System(Graph graph, float stiffness, float repulsion, float damping, float gravity) {
+            Stiffness = stiffness;
+            Repulsion = repulsion;
+            Damping = damping;
+            Threshold = 0.01f;
+            Graph = new Graph();
+
+            var nodes = new Dictionary<Node, PhysicsNode>();
+            foreach (var node in graph.Nodes) {
+                var physicsNode = new PhysicsNode(node);
+                Graph.Add(physicsNode);
+                nodes.Add(node, physicsNode);
+            }
+
+            physicsNodes = nodes;
+
+            foreach (var node in nodes.Values) {
+                foreach ((var conn1, var conn2, var other) in Graph.GetNeighbors(node)) {
+                    if (!Graph.TryGetDirectedConnection(other, node, out Connection conn)) {
+                        var spring = (Spring)conn;
+                        Graph.AddConnection(node, other, new Spring(spring.n2, spring.n1, spring.Length, spring.K));
+                    } else {
+                        // one of these must be non-null, since we're iterating over all existing connections
+                        var s1 = conn1?.Strength();
+                        var s2 = conn2?.Strength();
+                        s1 ??= s2;
+                        s2 ??= s1;
+                        Graph.AddConnection(node, other, new Spring(node, nodes[other], (float)(s1 + s2) / 2f, Stiffness));
+                    }
+                }
+            }
+            Gravity = gravity;
+        }
+
+        void ApplyCoulombsLaw() {
+            foreach (var n1 in Graph.Nodes.Cast<PhysicsNode>()) {
+                var p1 = n1.Point;
+                foreach (var n2 in Graph.Nodes.Cast<PhysicsNode>()) {
+                    if (ReferenceEquals(n1, n2)) {
+                        continue;
+                    }
+
+                    var p2 = n2.Point;
+                    var d = p1.Position - p2.Position;
+                    var dist = d.Magnitude();
+                    dist *= dist;
+                    var norm = d.Normalize();
+                    if (n1.Pinned && n2.Pinned) { } else if (n1.Pinned) {
+                        n2.ApplyForce(-norm * Repulsion / dist);
+                    } else if (n2.Pinned) {
+                        n1.ApplyForce(norm * Repulsion / dist);
+                    } else {
+                        n1.ApplyForce(norm * Repulsion / (dist * 0.5f));
+                        n2.ApplyForce(-norm * Repulsion / (dist * 0.5f));
+                    }
+                }
+            }
+        }
+
+        void ApplyHookesLaw() {
+            foreach (var spring in Graph.Connections.Cast<Spring>()) {
+                var n1 = spring.n1;
+                var n2 = spring.n2;
+                var d = spring.n1.Point.Position - spring.n2.Point.Position;
+                var displacement = d.Magnitude() * d.Magnitude() * Stiffness;
+                var direction = d.Normalize();
+
+                var force = direction * spring.K * displacement;
+
+                if (n1.Pinned && n2.Pinned) {
+                    
+                } else if (n1.Pinned) {
+                    n2.ApplyForce(force);
+                } else if (n2.Pinned) {
+                    n1.ApplyForce(-force);
+                } else {
+                    n1.ApplyForce(-force * 0.5f);
+                    n2.ApplyForce(force * 0.5f);
+                }
+            }
+        }
+
+        void AttractToCenter() {
+            foreach (var node in Graph.Nodes.Cast<PhysicsNode>()) {
+                if (!node.Pinned) {
+                    var d = node.Point.Position;
+                    var displacement = d.Magnitude();
+                    var direction = d.Normalize();
+                    node.ApplyForce(direction * (Stiffness * displacement * Gravity));
+                }
+            }
+        }
+
+        void UpdateVelocity(float timeStep) {
+            foreach (var node in Graph.Nodes.Cast<PhysicsNode>()) {
+                var point = node.Point;
+                point.Velocity += point.Acceleration * timeStep;
+                point.Velocity *= Damping;
+                point.Position += point.Velocity * timeStep;
+                point.Acceleration = Vector2.Zero;
+            }
+        }
+
+        float GetTotalEnergy() {
+            var total = 0.0f;
+            foreach (var node in Graph.Nodes.Cast<PhysicsNode>()) {
+                var speed = node.Point.Velocity.Magnitude();
+                total += 0.5f * node.Mass * speed * speed;
+            }
+            return total;
+        }
+
+        // Advance the simulation
+        public void Advance(float timeStep) {
+            ApplyCoulombsLaw();
+            ApplyHookesLaw();
+            AttractToCenter();
+            UpdateVelocity(timeStep);
+
+            WithinThreshold = GetTotalEnergy() < Threshold;
+        }
+    }
+}
