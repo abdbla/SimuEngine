c3b3abe94f287e3bd9996b3908f254027e7f1aaa
Abdbla <felixmurnion@gmail.com>
2020-09-25 11:48:32 +0200

    added stuff to group and connection

--- SimuEngine/Core/Group.cs
+++ SimuEngine/Core/Group.cs
@@ -1,8 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
 
 namespace Core {
-    public abstract class Group {
+    public abstract class Group
+    {
+        protected List<string> statuses;
+        protected Dictionary<string, int> traits;
+        protected List<Node> members;
+
+        public ReadOnlyDictionary<string, int> Traits {
+            get => new ReadOnlyDictionary<string, int>(traits);
+        }
+        public ReadOnlyCollection<string> Statuses {
+            get => statuses.AsReadOnly();
+        }
+        public ReadOnlyCollection<Node> Members {
+            get => members.AsReadOnly();
+        }
+
     }
 }
