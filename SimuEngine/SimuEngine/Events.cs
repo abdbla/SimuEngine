992a9e9c2c5650cd6b71dc25056493d3ee555883
Theodor Ström <look4totte@gmail.com>
2020-11-09 10:24:36 +0100

    Changed up Events.cs a bit
    Made it use auto-properties and added initial values to them. Added a
    constructor that takes all the functions. Added a class ActionEvent
    that is always guaranteed to occur and is only initialised with an outcome.

--- SimuEngine/SimuEngine/Events.cs
+++ SimuEngine/SimuEngine/Events.cs
@@ -25,37 +25,42 @@ namespace SimuEngine {
 
     public class Event {
         //The function takes the source node which triggered the event, the Graph it's contained in, and the top-level graph. It returns a bool if the requirement is fulfilled.
-        List<Func<Node, Graph, Graph, double>> reqPossible;
-        List<Func<Node, Graph, Graph, bool>> reqGuaranteed;
-        List<Action<Node, Graph, Graph>> outcome;
+        // List<Func<Node, Graph, Graph, double>> reqPossible;
+        // List<Func<Node, Graph, Graph, bool>> reqGuaranteed;
+        // List<Action<Node, Graph, Graph>> outcome;
 
         public List<Func<Node, Graph, Graph, double>> ReqPossible {
-            get { return reqPossible; }
-            set { }
-        }
+            get; private set;
+        } = new List<Func<Node, Graph, Graph, double>>();
         public List<Func<Node, Graph, Graph, bool>> ReqGuaranteed {
-            get { return reqGuaranteed; }
-            set { }
-        }
+            get; private set;
+        } = new List<Func<Node, Graph, Graph, bool>>();
         public List<Action<Node, Graph, Graph>> Outcome {
-            get { return outcome; }
-            set { }
-        }
+            get; private set;
+        } = new List<Action<Node, Graph, Graph>>();
 
         public void AddReqPossible(Func<Node, Graph, Graph, double> req) {
-            reqPossible.Add(req);
+            ReqPossible.Add(req);
         }
         public void AddReqGuaranteed(Func<Node, Graph, Graph, bool> req) {
-            reqGuaranteed.Add(req);
+            ReqGuaranteed.Add(req);
         }
         public void AddOutcome(Action<Node, Graph, Graph> ev) {
-            outcome.Add(ev);
+            Outcome.Add(ev);
         }
 
-        public Event() {
-            reqPossible = new List<Func<Node, Graph, Graph, double>>();
-            reqGuaranteed = new List<Func<Node, Graph, Graph, bool>>();
-            outcome = new List<Action<Node, Graph, Graph>>();
+        public Event() { }
+
+        public Event(Func<Node, Graph, Graph, double> reqPossible,
+                     Func<Node, Graph, Graph, bool> reqGuaranteed,
+                     Action<Node, Graph, Graph> outcome) {
+            ReqPossible.Add(reqPossible);
+            ReqGuaranteed.Add(reqGuaranteed);
+            Outcome.Add(outcome);
         }
+    }
+
+    public class ActionEvent : Event {
+        public ActionEvent(Action<Node, Graph, Graph> outcome) : base((n, l, w) => 1, (n, l, w) => true, outcome) { }
     }
 }
