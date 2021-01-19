97ba4630873442a8adfe50ce0e09604d8586d085
Sakulsan <Lukasoboy5@gmail.com>
2021-01-19 11:23:18 +0100

    Moved more stuff into Event.cs

--- SimuEngine/Implementation/Event.cs
+++ SimuEngine/Implementation/Event.cs
@@ -30,6 +30,8 @@ namespace Implementation
             get; private set;
         } = new List<Action<Node, Graph, Graph>>();
 
+
+
         public void AddReqPossible(Func<Node, Graph, Graph, double> req)
         {
             ReqPossible.Add(req);
@@ -52,6 +54,64 @@ namespace Implementation
             if (reqPossible != null) ReqPossible.Add(reqPossible);
             if (reqGuaranteed != null) ReqGuaranteed.Add(reqGuaranteed);
             Outcome.Add(outcome);
+        }
+
+        public static List<Event> InitializeEvents()
+        {
+            List<Event> personEvents = new List<Event>();
+
+            personEvents.Add(new Event());
+            personEvents[0].AddReqPossible(delegate (Node n, Graph l, Graph w) {
+                double chance = 0;
+                if (!n.Statuses.Contains("Healthy")) return 0;
+                foreach ((Connection, Node) m in l.GetOutgoingConnections(n))
+                {
+                    if (m.Item2.Statuses.Contains("Infected"))
+                    {
+                        chance += (double)((m.Item1.Traits["Proximity"]) + (double)((100 - m.Item2.Traits["Hygiene"])) + (double)((100 - n.Traits["Hygiene"])) / 300);
+                    }
+                }
+                return chance;
+            });
+            personEvents[0].AddOutcome(delegate (Node n, Graph l, Graph w) {
+                n.statuses.Remove("Healthy");
+                n.statuses.Add("Infected");
+            });
+
+            personEvents.Add(new Event());
+            personEvents[1].AddReqPossible(delegate (Node n, Graph l, Graph w) {
+                double chance = 0;
+                if (!n.Statuses.Contains("Infected")) return 0;
+                if (n.Statuses.Contains("Asthmatic")) chance += 0.1;
+                chance += ((double)n.Traits["Age"] / 150d);
+                return chance;
+            });
+            personEvents[1].AddOutcome(delegate (Node n, Graph l, Graph w) {
+                n.statuses.Remove("Infected");
+                n.statuses.Add("Dead");
+            });
+
+            personEvents.Add(new Event());
+            personEvents[2].AddReqPossible(delegate (Node n, Graph l, Graph w) {
+                double chance = 0;
+                if (!n.Statuses.Contains("Infected")) return 0;
+                chance = Math.Pow((101 - n.Traits["Age"]) / 100, 14 - n.Traits["Infected Time"]);
+                return chance;
+            });
+            personEvents[2].AddOutcome(delegate (Node n, Graph l, Graph w) {
+                n.statuses.Remove("Infected");
+                n.statuses.Add("Recovered");
+            });
+
+            personEvents.Add(new Event());
+            personEvents[3].AddReqGuaranteed(delegate (Node n, Graph l, Graph w) {
+                return n.Statuses.Contains("Infected") ? true : false;
+            });
+            personEvents[3].AddOutcome(delegate (Node n, Graph l, Graph w) {
+                n.traits["Infected Time"]++;
+            });
+
+            return personEvents;
         }
     }
 
--- SimuEngine/Implementation/Nodes.cs
+++ SimuEngine/Implementation/Nodes.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using Core;
 using SimuEngine;
 
+
 namespace Implementation {
     [Serializable]
     class City : Node
@@ -133,59 +134,6 @@ namespace Implementation {
             return;
         }
 
-        public static List<Event> InitializeEvents() {
-            List<Event> personEvents = new List<Event>();
-
-            personEvents.Add(new Event());
-            personEvents[0].AddReqPossible(delegate (Node n, Graph l, Graph w) {
-                double chance = 0;
-                if (!n.Statuses.Contains("Healthy")) return 0;
-                foreach ((Connection, Node) m in l.GetOutgoingConnections(n)) {
-                    if (m.Item2.Statuses.Contains("Infected")) {
-                        chance += (double)((m.Item1.Traits["Proximity"]) + (double)((100 - m.Item2.Traits["Hygiene"])) + (double)((100 - n.Traits["Hygiene"])) / 300);
-                    }
-                }
-                return chance;
-            });
-            personEvents[0].AddOutcome(delegate (Node n, Graph l, Graph w) {
-                n.statuses.Remove("Healthy");
-                n.statuses.Add("Infected");
-            });
-
-            personEvents.Add(new Event());
-            personEvents[1].AddReqPossible(delegate (Node n, Graph l, Graph w) {
-                double chance = 0;
-                if (!n.Statuses.Contains("Infected")) return 0;
-                if (n.Statuses.Contains("Asthmatic")) chance += 0.1;
-                chance += ((double)n.Traits["Age"] / 150d);
-                return chance;
-            });
-            personEvents[1].AddOutcome(delegate (Node n, Graph l, Graph w) {
-                n.statuses.Remove("Infected");
-                n.statuses.Add("Dead");
-            });
-
-            personEvents.Add(new Event());
-            personEvents[2].AddReqPossible(delegate (Node n, Graph l, Graph w) {
-                double chance = 0;
-                if (!n.Statuses.Contains("Infected")) return 0;
-                chance = Math.Pow((101 - n.Traits["Age"]) / 100, 14 - n.Traits["Infected Time"]);
-                return chance;
-            });
-            personEvents[2].AddOutcome(delegate (Node n, Graph l, Graph w) {
-                n.statuses.Remove("Infected");
-                n.statuses.Add("Recovered");
-            });
-
-            personEvents.Add(new Event());
-            personEvents[3].AddReqGuaranteed(delegate (Node n, Graph l, Graph w) {
-                return n.Statuses.Contains("Infected") ? true : false;
-            });
-            personEvents[3].AddOutcome(delegate (Node n, Graph l, Graph w) {
-                n.traits["Infected Time"]++;
-            });
-
-            return personEvents;
-        }
+        
     }
 }
